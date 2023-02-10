dcddc7d6bae9261b2206dd0e52176b8cb10cb2ac
Thomas Aleknovic <aleknovic70@gmail.com>
2023-02-10 16:30:22 -0300

    first commit

---
+++ src/test/java/com/thomas/backendtestattronatus/BackendTestAttronatusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thomas.backendtestattronatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTestAttronatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
